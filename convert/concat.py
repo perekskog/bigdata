11fa7c5d60d874bef326665eed789955ae34cd3b
Per Ekskog <per.ekskog@gmail.com>
2017-02-02 17:04:22 +0100

    Generalized concat to any number of input files

--- convert/concat.py
+++ convert/concat.py
@@ -25,17 +25,24 @@ def datetime_parser(json_dict):
     return json_dict
 
 
-def main(filein1, filein2, fileout):
-    items1 = json.load(open(filein1, 'r', encoding="utf-8"), object_hook=datetime_parser)
-    items2 = json.load(open(filein2, 'r', encoding="utf-8"), object_hook=datetime_parser)
-    items = items1 + items2
-    open(fileout, 'wb').write(json.dumps(items, sort_keys=False, indent=4, ensure_ascii=False)
+def main(fileargs):
+    outputfile = fileargs[len(fileargs)-1]
+    inputfiles = fileargs[:len(fileargs)-1]
+    print(outputfile)
+    print(inputfiles)
+
+    items = list()
+    for f in inputfiles:
+        newitems = json.load(open(f, 'r', encoding="utf-8"), object_hook=datetime_parser)
+        items += newitems
+
+    open(outputfile, 'wb').write(json.dumps(items, sort_keys=False, indent=4, ensure_ascii=False)
                               .encode('utf8'))
 
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 4:
-        main(sys.argv[1], sys.argv[2], sys.argv[3])
+    if len(sys.argv) >= 3:
+        main(sys.argv[1:])
     else:
         print("{}".format(__doc__))
