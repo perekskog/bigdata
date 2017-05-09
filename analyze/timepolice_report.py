34a22f1823c8c13bf1517473e5f0e74f1563c950
Per Ekskog <per.ekskog@gmail.com>
2017-05-09 19:59:07 +0200

    Refactored how distrubution of tasks are defined when analyzing data. Now the distribution is defined using a JSON file.

--- analyze/timepolice_report.py
+++ analyze/timepolice_report.py
@@ -3,14 +3,15 @@
 
 Usage:
 
-    timepolice_report store report firstdata lastdate
+    timepolice_report store report firstdata lastdate distribution
 
 Args:
     store   JSON file with timepolice data
-    report  
+    report
         timesheet   Summary of sessions of type "Kostnad"
     firstdata   First date to include, yy-mm-dd
     lastdate    Last date to include, yy-mm-dd
+    distribution    JSON file defining how tasks should be distributed
 
 """
 import sys
@@ -44,55 +45,48 @@ def session_between_dates(session, projectname, startdate, enddate):
         return False
 
 
-#        s = { x for x in task_summary.keys()}
-#        tasknames.update(s)
-
 def add_task(summary, name, time):
     try:
         summary[name] += time
     except KeyError:
         summary[name] = time
 
+def get_distribution(distributions, start):
+    dist = []
+    for d in reversed(distributions):
+        if d['start_date'] < start:
+            dist = d['distribution']
+            break
+    return dist
 
-def session_summary(session):
-#    task_summary = { t['taskname']: timedelta(0) for t in session['taskentries'] }
+def session_summary(session, distributions):
     summary = dict()
-    version_170213 = date(2017, 2, 12).toordinal()
 
     for taskentry in session['taskentries']:
         name = taskentry['taskname']
+        distribution = get_distribution(distributions, taskentry['start'])
+        # print(distribution)
         totaltime = taskentry['stop']-taskentry['start']
-        # if date is < 170213
-        if taskentry['start'].toordinal() <= version_170213:
-            if name == "STOD dok (???)":
-                add_task(summary, "STOD Vz", totaltime*0.67)
-                add_task(summary, "CMCC 9480 4018", totaltime*0.33)
-            else:
-                add_task(summary, name, totaltime)
-        # if date >= 170213
-        else:
-            if name == "STOD dok":
-                add_task(summary, "STOD Vz 7483", totaltime*0.67)
-                add_task(summary, "CMCC 4018", totaltime*0.33)
-            elif name == "ACoS misc":
-                add_task(summary, "TR CSR (m) 6718", totaltime*0.15)
-                add_task(summary, "TR TTX (a) 9342", totaltime*0.15)
-                add_task(summary, "DFU 2672", totaltime*0.7)
-            else:
-                add_task(summary, name, totaltime)
-        #    "STOD dok"  => ...
-        #    "ACoS misc" => ...
+        found = False
+        for origin in distribution:
+            if origin['origin_task'] == name:
+                for new in origin['distribute_as']:
+                    add_task(summary, new['new_task'], totaltime * new['new_amount'])
+                found = True
+                break
+        if found == False:
+            add_task(summary, name, totaltime)
     return summary
 
 
-def timesheet(store, first_date, last_date):
+def timesheet(store, first_date, last_date, distribution):
     first = datetime.strptime(first_date, "%y-%m-%d").date().toordinal()
     last = datetime.strptime(last_date, "%y-%m-%d").date().toordinal()
     subset_kostnad = [s for s in store if session_between_dates(s, "Kostnad", first, last)]
 
     tasknames = set()
     for session in subset_kostnad:
-        summary = session_summary(session)
+        summary = session_summary(session, distribution)
         print(session['date_created'].date())
         for task in summary:
             print(task, round(summary[task].seconds/3600, 2))
@@ -102,14 +96,16 @@ def timesheet(store, first_date, last_date):
         print()
 
 
-def main(store, report, first_date, last_date):
-    items = json.load(open(store, 'r', encoding="utf-8"), object_hook=datetime_parser)
+def main(store, report, first_date, last_date, distribution):
+    storeitems = json.load(open(store, 'r', encoding="utf-8"), object_hook=datetime_parser)
     if report == "timesheet":
-        timesheet(items, first_date, last_date)
+        distributionitems = json.load(open(distribution, 'r', encoding="utf-8"),
+                                      object_hook=datetime_parser)
+        timesheet(storeitems, first_date, last_date, distributionitems)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 5:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    if len(sys.argv) == 6:
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
     else:
         print("{}".format(__doc__))
